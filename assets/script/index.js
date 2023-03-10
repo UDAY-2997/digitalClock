cdab03c28c171c3c29e2591978f7f399d844b878
U-Dev <udaythakur.shely@gmail.com>
2023-03-10 15:23:09 -0600

    clock js added

--- assets/script/index.js
+++ assets/script/index.js
@@ -4,6 +4,10 @@ const time = document.querySelector(".time");
 const dateOn = document.querySelector(".date");
 const alarm = document.querySelector(".alarm");
 
+let alarmTime = null;
+let alarmAudio = new Audio("./assets/audio/alarm.mp3");
+alarmAudio.type = "audio/mp3";
+
 function digitalwatch() {
   const date = new Date();
   const day = date.getDate().toString().padStart(2, "0");
@@ -19,7 +23,37 @@ function digitalwatch() {
   time.innerHTML = todayTime;
   dateOn.innerHTML = todayDate;
 
+  if (alarmTime === `${hours}:${minutes}`) {
+    alarmAudio.play();
+    time.style.color = "red";
+  } else {
+    time.style.color = "#fcfcfc";
+  }
+
   setTimeout(digitalwatch, 1000);
 }
+
+document.querySelector(".form").addEventListener("submit", function (event) {
+  event.preventDefault();
+  const alarmTimeInput = document.querySelector(".alarm-time");
+  const alarmTimeDisplay = document.querySelector(".alarm-time-display");
+  const inputRegex = /^([01]?[0-9]|2[0-3]):[0-5][0-9]$/;
+  if (inputRegex.test(alarmTimeInput.value)) {
+    alarmTime = alarmTimeInput.value;
+    alarmTimeDisplay.textContent = alarmTime;
+    alarmTimeInput.value = "";
+  } else {
+    alert("Please enter a valid time in HH:MM format.");
+    alarmTimeInput.value = "";
+  }
+});
+
+document.querySelector(".form").addEventListener("reset", function (event) {
+  event.preventDefault();
+  alarmTime = null;
+  document.querySelector(".alarm-time-display").textContent = "";
+  alarmAudio.pause();
+  alarmAudio.currentTime = 0;
+});
 
 digitalwatch();
